c8686bbf7cc538613dd4aac238e8592b2a8d96d1
MRINAL RAJAK <mrinalrajak123@gmail.com>
2022-08-15 17:56:21 +0530

    Python

---
+++ Bessel_id11.py
@@ -0,0 +1,26 @@
+
+#Bessel's identity
+#(11)
+
+import numpy as np
+from scipy.special import jn
+from scipy.integrate import quad
+from scipy.misc import derivative
+
+x=float(input("Enter the x: "))
+n=1
+theta=int(input("Enter the theta: "))
+LHS=np.cos(x*np.cos(theta))
+term=2*(-1)**n*jn(2*n,x)*np.cos(2*n*theta)
+RHS=jn(0,x)+term
+s=0.0
+while(abs(LHS-RHS)>1.0e-5):
+    term=2*(-1)**n*jn(2*n,x)*np.cos(2*n*theta)
+    s=s+term
+    RHS=jn(0,x)+s
+    n=n+1
+
+
+print("LHS: ",LHS)
+print("RHS: ",RHS)
+    
